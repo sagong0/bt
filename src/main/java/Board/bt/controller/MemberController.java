4c41e51c33136c35e3a69bea7bdf33e54856926b
sagong0 <1w2q3e@naver.com>
2024-01-24 22:05:18 +0900

    MemberController , join.html

---
+++ src/main/java/Board/bt/controller/MemberController.java
@@ -0,0 +1,20 @@
+package Board.bt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("/member/add")
+    public String memberFormCreate(){
+        return "member/join";
+    }
+
+    @PostMapping("/member/add")
+    public String memberForm(){
+        return "member/";
+    }
+}
