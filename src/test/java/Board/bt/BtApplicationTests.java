f85afb7472af0cc98f3e9f11e76ff0f6ba13fb3f
sagong0 <1w2q3e@naver.com>
2024-01-24 16:08:29 +0900

    first

---
+++ src/test/java/Board/bt/BtApplicationTests.java
@@ -0,0 +1,13 @@
+package Board.bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
