a6e372dbcd20015f27c6b5e1d8c82c0d94137f11
Jung ho Jeon <wjswjdgh03@gmail.com>
2024-01-24 22:24:45 +0900

    클래스 세분화

--- src/main/java/Board/bt/repository/member/MemberRepositoryImpl.java
+++ src/main/java/Board/bt/repository/member/MemberRepositoryImpl.java
@@ -1,6 +1,7 @@
-package Board.bt.repository;
+package Board.bt.repository.member;
 
 import Board.bt.domain.Member;
+import Board.bt.repository.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
